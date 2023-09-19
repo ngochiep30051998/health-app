7b88aa4a82dfbd843bbca15b63ad330aff4642fa
ngochiep30051998 <ngochiep30051998@Gmail.com>
2023-09-19 23:04:18 +0700

    layout

--- src/components/layouts/InLoginLayout.tsx
+++ src/components/layouts/InLoginLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./header/Header";
 
 export default function InLoginLayout(){
-    return <div className="container w-full">
+    return <div className="w-full">
         <Header />
         <Outlet />
     </div>

1d3567a7f978c52e1544729848b4e4e9f64cc6ff
ngochiep30051998 <35429836+ngochiep30051998@users.noreply.github.com>
2023-09-22 08:25:27 +0000

    btn action

--- src/components/button-view-more/ButtonViewMore.tsx
+++ src/components/button-view-more/ButtonViewMore.tsx
@@ -1,6 +1,9 @@
-import "./ButtonViewMore.scss"
-export default function ButtonViewMore() {
-    return <div className="w-full h-14 btn-view-more flex justify-center items-center">
+import "./ButtonViewMore.scss";
+type props = {
+    onViewMore?: () => void;
+}
+export default function ButtonViewMore({ onViewMore }: props) {
+    return <div className="w-full h-14 btn-view-more flex justify-center items-center" onClick={onViewMore}>
         <button className="h-full color-light font-normal rounded">記録をもっと見る</button>
     </div>
 }
